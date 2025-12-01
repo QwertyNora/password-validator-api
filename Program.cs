4797a496a3f56f82b6de06c407928cbc9bf10134
Nora Silfver <nora.silfver@gmail.com>
2025-12-01 09:39:03 +0100

    Register PasswordService in the dependency injection container

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 using PasswordValidatorApi.Data;
 using Microsoft.EntityFrameworkCore;
+using PasswordValidatorApi.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddScoped<PasswordService>();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
